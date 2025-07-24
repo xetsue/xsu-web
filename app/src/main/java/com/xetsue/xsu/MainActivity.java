2a6105e1014c35dd071331c2af3ee5e2a98a8432
xetsue <eudesmer99@gmail.com>
2025-07-24 23:31:39 +0800

    Initial Commit

---
+++ app/src/main/java/com/xetsue/xsu/MainActivity.java
@@ -0,0 +1,39 @@
+package com.xetsue.xsu;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+
+            myWeb = findViewById(R.id.myWeb);
+
+            myWeb.getSettings().setJavaScriptEnabled(true);
+            myWeb.setWebViewClient(new WebViewClient());
+            myWeb.loadUrl("https://xetsue.github.io/");
+
+            return insets;
+        });
+    }
+}
